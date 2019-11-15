b55096f20f8297c6d91b41933bb12bee0a694985
sudhakar <sudhakarbedapudi@gmail.com>
2019-11-15 11:23:01 +0530

    mocha "function called or not"  implemented

--- test/test.js
+++ test/test.js
@@ -3,6 +3,11 @@ var sinon = require('sinon');
 var actions = require('../app/actions')
 var lib = require('../app/lib')
 var utils = require('../app/utils')
+var chai      = require('chai');
+//var sinonChai = require('sinonChai');
+var expect    = chai.expect;
+
+//chai.use('sinonChai');
 
 /* var now = new Date(2017, 3, 12);
 
@@ -38,18 +43,33 @@ describe("actions", ()=>{
     it('actions this month', () => {
         assert.equal(actions.getMonth(), 9)
     }) */
-    it('welcomeMessage shd say hello', ()=> {
+    /* it('welcomeMessage shd say hello', ()=> {
         sinon.stub(lib, 'sayHi').callsFake(function() {
             return "hello"
         });
         assert.equal(actions.welcomeMessage(), 'hello')
         lib.sayHi.restore(); // restore original functionality
         
-    })
+    }) */
+
+
+  var method;
+
+  beforeEach(function (done) {
+    method = sinon.spy(utils.prototype, 'getData');
+    done();
+  });
+  afterEach(function (done) {
+    method.restore();
+    done();
+  });
 
     it('object method mocking', ()=> {
-        sinon.stub(utils.prototype, 'getData').callsFake(() => {return 'abc'});
-        assert.equal(actions.toBeTestedForObjectMocking(), 'data is abc')
+        //sinon.stub(utils.prototype, 'getData').callsFake(() => {return 'abc'});
+        //assert.equal(actions.toBeTestedForObjectMocking(), 'data is abc')
+        actions.toBeTestedForObjectMocking()
+        console.log("here",method.called)
+        //expect(method).to.have.been.called;
         
     })
 })
